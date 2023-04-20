fa6e790037fb6d71d1c500ebd45c3aba6d7e2cd7
Zeeshan Ahmad <za094270@gmail.com>
2023-04-20 15:12:29 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Attendance System</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/style.css">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/css/bootstrap.min.css" rel="stylesheet">
+        
+    </head>
+    <body>
+
+
+        <section class="mainpage" style="background-image: url('assets/img/login-bgimg.jpeg'); background-position:center; background-color: grey; background-blend-mode: multiply; background-repeat:no-repeat; background-size:cover; width:100%;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <a href="admin/login.php">
+                            <div class="cards">
+                                <img src="assets/img/admin.png" alt="">
+                                <h2>Admin</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <a href="teacher/login.php">
+                            <div class="cards">
+                                <img src="assets/img/teacher.png" alt="">
+                                <h2>Teacher</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <a href="student/login.php">
+                            <div class="cards">
+                                <img src="assets/img/student.png" alt="">
+                                <h2>Student</h2>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/js/bootstrap.min.js"></script>
+
+    </body>
+</html>
